e02201ad02e5e58580c7c749f25691694cf1008c
mike <mike@9de07d3f-d96d-4749-a6ee-ac564a2828a3>
2009-04-30 16:13:44 +0000

    git-svn-id: svn://10.0.50.55/dungeonlegion/trunk@45 9de07d3f-d96d-4749-a6ee-ac564a2828a3

--- make.bat
+++
@@ -1,1 +0,0 @@
-g++ -DALLEGRO_STATICLINK src/Main.cpp src/PolyMap/PolyMap.cpp src/AllegroGraphics/Graphics.cpp -o DungeonLegion.exe -I src src/Math/Real.cpp src/Math/Vector/Vector2D.cpp src/Math/Matrix/Matrix2x2.cpp src/Math/Matrix/Matrix3x3.cpp -lalleg_s -lkernel32 -luser32 -lgdi32 -lcomdlg32 -lole32 -ldinput -lddraw -ldxguid -lwinmm -ldsound
