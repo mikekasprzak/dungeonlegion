d3b8ea96c3b76e133f85c5862d82404705c6eb18
mike <mike@9de07d3f-d96d-4749-a6ee-ac564a2828a3>
2009-05-18 11:49:45 +0000

    More restructure.  Unfinished tests now live in a "TODO" folder.  En mass include headers added too.
    git-svn-id: svn://10.0.50.55/dungeonlegion/trunk@51 9de07d3f-d96d-4749-a6ee-ac564a2828a3

---
+++ src/AdvancedGeometry/2D/Point.h
@@ -0,0 +1,21 @@
+// - ------------------------------------------------------------------------------------------ - //
+#ifndef __AdvancedGeometry_2D_Point_H__
+#define __AdvancedGeometry_2D_Point_H__
+// - ------------------------------------------------------------------------------------------ - //
+
+#include "Point/PointVsPairRect2D.h"
+#include "Point/PointVsShapeRect2D.h"
+#include "Point/PointVsRadiusRect2D.h"
+
+#include "Point/PointVsSphere2D.h"
+#include "Point/PointVsCapsule2D.h"
+#include "Point/PointVsCapsuleChain2D.h"
+
+#include "Point/PointVsChain2D.h"
+
+#include "Point/PointVsPointCluster2D.h"
+#include "Point/PointVsPolygon2D.h"
+
+// - ------------------------------------------------------------------------------------------ - //
+#endif // __AdvancedGeometry_2D_Point_H__ //
+// - ------------------------------------------------------------------------------------------ - //
