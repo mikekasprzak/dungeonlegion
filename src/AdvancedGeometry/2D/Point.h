206f6013e2a12c0ebb0c898cbe9156a122b67eb1
mike <mike@9de07d3f-d96d-4749-a6ee-ac564a2828a3>
2009-05-18 12:01:45 +0000

    Fixed the point includes.
    git-svn-id: svn://10.0.50.55/dungeonlegion/trunk@52 9de07d3f-d96d-4749-a6ee-ac564a2828a3

--- src/AdvancedGeometry/2D/Point.h
+++ src/AdvancedGeometry/2D/Point.h
@@ -2,20 +2,24 @@
 #ifndef __AdvancedGeometry_2D_Point_H__
 #define __AdvancedGeometry_2D_Point_H__
 // - ------------------------------------------------------------------------------------------ - //
-
+// Rectangles //
 #include "Point/PointVsPairRect2D.h"
 #include "Point/PointVsShapeRect2D.h"
 #include "Point/PointVsRadiusRect2D.h"
-
+// - ------------------------------------------------------------------------------------------ - //
+// Sphere, Capsule, and a chain of capsules //
 #include "Point/PointVsSphere2D.h"
 #include "Point/PointVsCapsule2D.h"
 #include "Point/PointVsCapsuleChain2D.h"
-
+// - ------------------------------------------------------------------------------------------ - //
+// Line, Chain (lines) //
+#include "Point/PointVsLine2D.h"
 #include "Point/PointVsChain2D.h"
-
+// - ------------------------------------------------------------------------------------------ - //
+// Point Cluster, Polygon, Edged Polygon (one with edge enability flags) //
 #include "Point/PointVsPointCluster2D.h"
 #include "Point/PointVsPolygon2D.h"
-
+#include "Point/PointVsEdgedPolygon2D.h"
 // - ------------------------------------------------------------------------------------------ - //
 #endif // __AdvancedGeometry_2D_Point_H__ //
 // - ------------------------------------------------------------------------------------------ - //
