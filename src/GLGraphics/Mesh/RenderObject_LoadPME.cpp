75cc01fe804a1d37b3e09014e6c19bd0737779eb
mike <mike@9de07d3f-d96d-4749-a6ee-ac564a2828a3>
2009-05-28 15:30:21 +0000

    mostly done the PME to RO converter.  Still finishing the faces.
    git-svn-id: svn://10.0.50.55/dungeonlegion/trunk@102 9de07d3f-d96d-4749-a6ee-ac564a2828a3

--- src/GLGraphics/Mesh/RenderObject_LoadPME.cpp
+++ src/GLGraphics/Mesh/RenderObject_LoadPME.cpp
@@ -1,11 +1,91 @@
 // - ------------------------------------------------------------------------------------------ - //
 #include "RenderObject.h"
 #include "PMEFile.h"
+
+#include <Texture/TexturePool.h>
 // - ------------------------------------------------------------------------------------------ - //
 cRenderObject LoadPME( const cPMEFile& PME ) {
 	cRenderObject Obj;
 	
-	
+	// Materials //
+	{
+		// Count Materials //
+		size_t MaterialCount = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			MaterialCount += PME.Mesh[idx].Material.size();
+		}
+		
+		Obj.Material = new_Array<cROMaterial>(MaterialCount);
+		size_t CurrentMaterial = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			for ( size_t idx2 = 0; idx2 < PME.Mesh[idx].Material.size(); idx2++ ) {
+//				Obj.Material->Data[CurrentMaterial].Texture = gfxLoadTexture( PME.Mesh[idx].Material[idx2].ImageFileName.c_str() );
+				Obj.Material->Data[CurrentMaterial].Texture = 0;
+				
+				CurrentMaterial++;
+			}
+		}
+	}
+
+	// Vertices //
+	{
+		// Count Vertices //
+		size_t VertexCount = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			VertexCount += PME.Mesh[idx].Vertex.size();
+		}
+
+		Obj.Vertex = new_Array<cROVertex>(VertexCount);
+		size_t VertexOffset = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			for ( size_t idx2 = 0; idx2 < PME.Mesh[idx].Vertex.size(); idx2++ ) {
+				cROVertex& ROVert = Obj.Vertex->Data[VertexOffset + idx2];
+				const cPMEVertex& PMEVert = PME.Mesh[idx].Vertex[idx2];
+				
+				// Broken up per component, in case the vertex types are actually different //
+				ROVert.Pos.x = PMEVert.Pos.x;
+				ROVert.Pos.y = PMEVert.Pos.y;
+				ROVert.Pos.z = PMEVert.Pos.z;
+                
+				ROVert.Normal.x = PMEVert.Normal.x;
+				ROVert.Normal.y = PMEVert.Normal.y;
+				ROVert.Normal.z = PMEVert.Normal.z;
+                
+                ROVert.UV.u = PMEVert.UV.u;
+				ROVert.UV.v = PMEVert.UV.v;
+                
+                ROVert.Color = PMEVert.Color;
+			}
+			VertexOffset += PME.Mesh[idx].Vertex.size();
+		}
+	}
+
+	// FaceGroups //
+	{
+		// Count Vertices //
+		size_t FaceGroupCount = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			FaceGroupCount += PME.Mesh[idx].FaceGroup.size();
+		}
+		
+		Obj.FaceGroup = new_Array<cROFaceGroup>(FaceGroupCount);
+		size_t MaterialOffset = 0;
+		size_t FaceGroupOffset = 0;
+		for ( size_t idx = 0; idx < PME.Mesh.size(); idx++ ) {
+			for ( size_t idx2 = 0; idx2 < PME.Mesh[idx].FaceGroup.size(); idx2++ ) {
+				cROFaceGroup& ROFaceGroup = Obj.FaceGroup->Data[FaceGroupOffset + idx2];
+				const cPMEFaceGroup& PMEFaceGroup = PME.Mesh[idx].FaceGroup[idx2];
+				
+				ROFaceGroup.MaterialIndex = MaterialOffset + PMEFaceGroup.Material;
+				
+				// Broken up per component, in case the FaceGroup types are actually different //
+				
+
+			}
+			FaceGroupOffset += PME.Mesh[idx].FaceGroup.size();
+			MaterialOffset += PME.Mesh[idx].Material.size();
+		}		
+	}
 	
 	return Obj;
 }
